4b3a00c8510d4f1caddd1574b18aff50e521e367
Keith Suderman <suderman@anc.org>
2014-05-31 17:37:20 +0000

    Added the correct copyright notice to all source files.

--- src/main/java/org/anc/lapps/stanford/SANamedEntityRecognizer.java
+++ src/main/java/org/anc/lapps/stanford/SANamedEntityRecognizer.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright 2014 The Language Application Grid.
+ * Copyright 2014 The American National Corpus.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
